c57917d07926c3be41f536abbd431d4f75b36f4f
Ortal Vaknin <ortal@dynamicyield.com>
2020-05-24 16:14:52 +0300

    project

--- src/main/java/Entities/FlowRepository.java
+++
@@ -1,7 +0,0 @@
-package Entities;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface FlowRepository extends CrudRepository<FlowEntity, Long> {
-   FlowEntity findByFlowName(String flowName);
-}
